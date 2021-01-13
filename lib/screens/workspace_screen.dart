779d8fe3967f5e6dc90e49fb780b352f188dd19e
Ramzes5614 <46199138+Ramzes5614@users.noreply.github.com>
2021-01-13 20:17:04 +0300

    merged with guide branch

--- lib/screens/workspace_screen.dart
+++ lib/screens/workspace_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recycle_hub/bloc/navigation_bloc.dart';
-import 'package:recycle_hub/elements/drawer.dart';
-import 'package:recycle_hub/repo/eco_quide_repo.dart';
 import 'package:recycle_hub/screens/tabs/eco_coin/eco_coin_screen.dart';
-import 'package:recycle_hub/screens/tabs/eco_gide/eco_guide_tabs/main_eco_guide_screen.dart';
 import 'package:recycle_hub/screens/tabs/eco_gide/main_eco_screen.dart';
 import 'package:recycle_hub/screens/tabs/map/map_screen.dart';
 import 'package:recycle_hub/screens/tabs/profile/profile_screen.dart';
@@ -32,88 +29,9 @@ class _WorkSpaceState extends State<WorkSpaceScreen> {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Stack(children: [
-        StreamBuilder(
-          stream: bottomNavBarBloc.itemStream,
-          initialData: bottomNavBarBloc.defaultItem,
-          // ignore: missing_return
-          builder: (context, AsyncSnapshot<NavBarItem> snapshot) {
-            Widget child;
-            switch (snapshot.data) {
-              case NavBarItem.MAP:
-                return MapScreen();
-                break;
-              case NavBarItem.ECO_GIDE:
-                return EcoMainScreen();
-                break;
-              case NavBarItem.ECO_COIN:
-                return EcoCoinScreen();
-                break;
-              case NavBarItem.PROFILE:
-                return ProfileScreen();
-                break;
-            }
-          },
-        ),
-        //WorkSpaceScreen(),
-        _bottomNavBarAlignment(),
-      ]),
-    );
-  }
-}
-
-Align _bottomNavBarAlignment() {
-  return Align(
-    alignment: Alignment.bottomCenter,
-    child: Container(
-      height: 70,
-      child: StreamBuilder(
-          stream: bottomNavBarBloc.itemStream,
-          initialData: bottomNavBarBloc.defaultItem,
-          // ignore: missing_return
-          builder: (context, AsyncSnapshot<NavBarItem> snapshot) {
-            return BottomNavBarV2(
-              selectedIconThemeData:
-                  Theme.of(context).bottomNavigationBarTheme.selectedIconTheme,
-              unselectedIconThemeData: Theme.of(context)
-                  .bottomNavigationBarTheme
-                  .unselectedIconTheme,
-              backgraundColor: Theme.of(context).backgroundColor,
-              currentItem: snapshot.data.index,
-            );
-          }),
-    ),
-  );
-}
-/*Scaffold(
       appBar: mapScreenAppBar(context),
-      /*PreferredSize(
-        preferredSize: const Size.fromHeight(70),
-        child: StreamBuilder(
-          stream: bottomNavBarBloc.itemStream,
-          initialData: bottomNavBarBloc.defaultItem,
-          // ignore: missing_return
-          builder: (context, AsyncSnapshot<NavBarItem> snapshot) {
-            switch (snapshot.data) {
-              case NavBarItem.MAP:
-                return mapScreenAppBar(context);
-                break;
-              case NavBarItem.ECO_GIDE:
-                return ecoGuideScreenAppBer();
-                break;
-              case NavBarItem.ECO_COIN:
-                return ecoCoinScreenAppBar();
-                break;
-              case NavBarItem.PROFILE:
-                return profileScreenAppBar();
-                break;
-            }
-          },
-        ),
-      ),*/
-      drawer: customDrawer,
-      body: googleMap(context),
-      /*Stack(children: [
+      drawer: mapScreenDrawer(),
+      body: Stack(children: [
         StreamBuilder(
           stream: bottomNavBarBloc.itemStream,
           initialData: bottomNavBarBloc.defaultItem,
@@ -135,8 +53,8 @@ Align _bottomNavBarAlignment() {
                 break;
             }
           },
-        )*/
-      /*Align(
+        ),
+        Align(
           alignment: Alignment.bottomCenter,
           child: Container(
             height: 70,
@@ -159,7 +77,7 @@ Align _bottomNavBarAlignment() {
                 }),
           ),
         ),
-      ]),*/
+      ]),
     );
   }
-}*/
+}
