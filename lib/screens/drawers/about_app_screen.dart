7b0f4a028509575f0c73ba03c53a648108140da5
Ramzes <46199138+Ramzes5614@users.noreply.github.com>
2021-11-02 01:39:49 +0300

    Added app version

--- lib/screens/drawers/about_app_screen.dart
+++ lib/screens/drawers/about_app_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:recycle_hub/elements/common_cell.dart';
+import 'package:recycle_hub/screens/tabs/map/widgets/loader_widget.dart';
 import 'about_developers_screen.dart';
+import 'package:package_info/package_info.dart';
 
 class AboutAppScreen extends StatefulWidget {
   @override
@@ -8,6 +10,15 @@ class AboutAppScreen extends StatefulWidget {
 }
 
 class _AboutAppScreenState extends State<AboutAppScreen> {
+  Future<String> _getPackage() async {
+    try {
+      PackageInfo packageInfo = await PackageInfo.fromPlatform();
+      return '${packageInfo.version} (${packageInfo.buildNumber})';
+    } catch (e) {
+      return '';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     print('helloabdu');
@@ -27,44 +38,67 @@ class _AboutAppScreenState extends State<AboutAppScreen> {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-        child: ListView(
-          shrinkWrap: true,
-          children: [
-            CommonCell(
-              text: 'Условия и положения',
-              onTap: () {
-                /* Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => BePartnerScreen(),
-                  ),
-                ); */
-              },
-            ),
-            CommonCell(
-              text: 'Политика конфеденциальности',
-              onTap: () {},
-            ),
-            CommonCell(
-              text: 'Условия реферальной программы',
-              onTap: () {},
-            ),
-            CommonCell(
-              text: 'О разработчиках',
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => AboutDevelopersScreen(),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
+      body: FutureBuilder<String>(
+          future: _getPackage(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.active || snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: LoaderWidget());
+            }
+            if (snapshot.hasData) {
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    ListView(
+                      shrinkWrap: true,
+                      children: [
+                        CommonCell(
+                          text: 'Условия и положения',
+                          onTap: () {
+                            /* Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BePartnerScreen(),
+                          ),
+                        ); */
+                          },
+                        ),
+                        CommonCell(
+                          text: 'Политика конфеденциальности',
+                          onTap: () {},
+                        ),
+                        CommonCell(
+                          text: 'Условия реферальной программы',
+                          onTap: () {},
+                        ),
+                        CommonCell(
+                          text: 'О разработчиках',
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => AboutDevelopersScreen(),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                    Positioned(
+                      right: 8,
+                      bottom: 16,
+                      child: Text(snapshot.data),
+                    )
+                  ],
+                ),
+              );
+            } else {
+              return Center(
+                child: LoaderWidget(),
+              );
+            }
+          }),
     );
   }
 }
